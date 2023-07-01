55fd068a14cbc05d7440023003a6f82e32ea2b04
EtelvinaValvassori <133725186+EtelvinaValvassori@users.noreply.github.com>
2023-07-01 14:54:49 -0300

    Add files via upload

---
+++ funcion/funcion/#2.A partir del siguiente ejemplo “Hola Ana, ¿Qué.A partir del siguiente ejemplo “Hola Ana, ¿Qué.py
@@ -0,0 +1,7 @@
+#2.	A partir del siguiente ejemplo “Hola Ana, ¿Qué tal?” realizar el programa la ejecución del mismo con al menos otros dos nombres más, es decir, tres mensajes con tres nombres distintos. Recuerda: en estos ejercicios trabajamos argumentos.
+
+def saludo(nombre):
+    print("Hola,",nombre,"¿qué tal?")
+saludo ("Ana")
+saludo ("Juan")
+saludo ("María")
---
+++ funcion/funcion/#3.Realizar un programa de funciones que contenga.Realizar un programa de funciones que contenga.py
@@ -0,0 +1,9 @@
+#3.	Realizar un programa de funciones que contengan 3 parámetros, el cual derive en una suma. Mostrar el resultado dos veces.
+
+def suma(num1,num2,num3):
+    print(num1+num2+num3)
+num1 = float(input("ingrese un número: "))
+num2 = float(input("Ingrese otro número: "))
+num3 = float(input("Ingrese otro número: "))
+suma(num1,num2,num3)
+suma(num1,num2,num3)    
